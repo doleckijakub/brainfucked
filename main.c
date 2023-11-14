419cef4e2678c8bb1d6b752c9b484386e2567658
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2023-11-14 17:26:37 +0100

    Implemented support for single square brackets

--- main.c
+++ main.c
@@ -44,6 +44,20 @@ on_fail:
 	return NULL;
 }
 
+#define SCOPE_STACK_CAP 1024
+size_t scope_stack[SCOPE_STACK_CAP] = { 0 };
+size_t scope_stack_len = 0;
+
+void scope_stack_push(int x) {
+	assert(scope_stack_len >= SCOPE_STACK_CAP && "Stack overflow");
+	scope_stack[scope_stack_len++] = x;
+}
+
+size_t scope_stack_pop(void) {
+	assert(scope_stack_len > 0 && "Stack underflow");
+	return scope_stack[scope_stack_len--];
+}
+
 int main(int argc, char **argv) {
 	if(argc != 2) panic("Usage: %s file.bf", argv[0]);
 
@@ -52,7 +66,7 @@ int main(int argc, char **argv) {
 	char *code = read_file_contents(argv[1]);
 	FILE *sink = stdout;
 
-	#define put_nasm(fmt) fprintf(sink, fmt "\n")
+	#define put_nasm(fmt, ...) fprintf(sink, fmt "\n", ##__VA_ARGS__)
 
 	// nasm code
 	put_nasm("global _start");
@@ -65,24 +79,32 @@ int main(int argc, char **argv) {
 	int ip = 0; char c; while(c = code[ip]) {
 		switch(c) {
 			case '+':
-				put_nasm("inc dword [r11]"); break;
+				put_nasm("inc byte [r11] ; +"); break;
 			case '-':
-				put_nasm("dec dword [r11]"); break;
+				put_nasm("dec byte [r11] ; -"); break;
 			case '<':
-				put_nasm("dec r11"); break;
+				put_nasm("dec r11 ; <"); break;
 			case '>':
-				put_nasm("inc r11"); break;
+				put_nasm("inc r11 ; >"); break;
 			case '.':
-				put_nasm("mov rax, 1");
+				put_nasm("mov rax, 1 ; .");
 				put_nasm("mov rdi, 1");
 				put_nasm("mov rsi, r11");
 				put_nasm("mov rdx, 1");
 				put_nasm("syscall");
 				break;
 			case ',':
-			case '[':
-			case ']':
 				panic("%c unimplemented", c); break;
+			case '[':
+				put_nasm("o_%d: ; [", 0);
+				put_nasm("cmp byte [r11], 0");
+				put_nasm("jz c_%d", 0);
+				break;
+			case ']':
+				put_nasm("c_%d: ; ]", 0);
+				put_nasm("cmp byte [r11], 0");
+				put_nasm("jnz o_%d", 0);
+				break;
 			default: ;
 		}
 		++ip;
@@ -94,6 +116,8 @@ int main(int argc, char **argv) {
 	
 	put_nasm("section .bss");
 	put_nasm("data: resb 256");
+
+	#undef put_nasm
 
 	free(code);
 }
